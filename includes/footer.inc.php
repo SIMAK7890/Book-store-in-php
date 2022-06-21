213d53dbacc04bf728b85a343d407e12525dbfae
SIMAK7890 <70663996+SIMAK7890@users.noreply.github.com>
2022-06-22 03:48:46 +0530

    Add files via upload

---
+++ includes/footer.inc.php
@@ -0,0 +1,3 @@
+<div id="footer-wrap">
+	<p id="legal"> Copyright &copy; 2019 Books.com . Design and maintained by <a href="http://www.gmail.com/mohdshakir30101999@gmail.com"> Mohd Shakir And Amir Dhawa.</a>.</p>
+	</div>
---
+++ successfully.php
@@ -0,0 +1,2 @@
+<p class="lead text-success">Your order has been processed sucessfully. Please check your email to get your order confirmation and shipping detail!. 
+	Your cart has been empty.</p>
